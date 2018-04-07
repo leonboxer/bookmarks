7ba9e456db2a0307968e39610940a48268d39731
leonboxer <boxers@126.com>
2018-04-07 10:35:30 +0800

    20180407

--- account/urls.py
+++ account/urls.py
@@ -37,5 +37,6 @@ urlpatterns = [
         'django.contrib.auth.views.password_reset_complete',
         name='password_reset_complete'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^registration$',views.register,name='register_done')
+    url(r'^registration$',views.register,name='register_done'),
+    url(r'^edit/$', views.edit, name='edit'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, \
+    UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 def user_login(request):
@@ -15,7 +17,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated ' 
+                    return HttpResponse('Authenticated '
                                         'successfully')
                 else:
                     return HttpResponse('Disabled account')
@@ -39,11 +41,13 @@ def register(request):
         if user_form.is_valid():
             # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-        # Set the chosen password
+            # Set the chosen password
             new_user.set_password(
                 user_form.cleaned_data['password'])
-        # Save the User object
+            # Save the User object
             new_user.save()
+            # Create the user profile
+            profile = Profile.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -52,3 +56,24 @@ def register(request):
     return render(request,
                   'account/register.html',
                   {'user_form': user_form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST,
+            files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile)
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
