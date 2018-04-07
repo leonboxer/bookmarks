37079fb80190cfab1c5bc5eab85851c8c0b94df2
leonboxer <boxers@126.com>
2018-04-07 22:03:28 +0800

    images

--- images/views.py
+++ images/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(requset):
+    if requset.method == 'POST':
+        # form is sent
+        form = ImageCreateForm(data=requset.POST)
+        if form.is_valid():
+            # form data is valid
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            # assign current user to the item
+            new_item.user = requset.user
+            new_item.save()
+            messages.success(requset, 'Image added successfully')
+
+            # redirect to new created item detail view
+            return redirect(new_item.get_absolute_url())
+    else:
+        # build form with data provided by the bookmaklet via GET
+        form = ImageCreateForm(data=requset.GET)
+    return render(requset,
+                  'images/image/create.html',
+                  {'sections': 'images',
+                   'form': form})
