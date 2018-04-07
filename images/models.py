a4f60466a8e24e0da8bf0ade4dde9c7b43b90568
leonboxer <boxers@126.com>
2018-04-07 20:48:32 +0800

    images

--- images/models.py
+++ images/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import  settings
+from django.conf import settings
 from django.utils.text import slugify
 
 
@@ -21,7 +21,7 @@ class Image(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-            super(Image,self).save(*args, **kwargs)
+            super(Image, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
