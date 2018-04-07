7bc0028403a214009093eb8e8f4497527aa18892
leonboxer <boxers@126.com>
2018-04-07 21:25:48 +0800

    images

--- settings.py
+++ settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'account',
-    'images',
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'account',
+    'images',
 )
 
 MIDDLEWARE_CLASSES = (
