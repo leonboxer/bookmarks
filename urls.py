8b5c5fecc84547914789cc0f07fc1f815e45e23d
leonboxer <boxers@126.com>
2018-04-08 17:46:54 +0800

    images

--- urls.py
+++ urls.py
@@ -18,10 +18,10 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),
+    url(r'^images/', include('images.urls', namespace='images')),
 ]
 
 if settings.DEBUG:
