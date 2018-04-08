c808b2c29aa4eb1cc6a424929c86840729091630
leonboxer <boxers@126.com>
2018-04-08 19:40:06 +0800

    images

--- images/views.py
+++ images/views.py
@@ -5,13 +5,13 @@ from .forms import ImageCreateForm
 
 
 @login_required
-def image_create(requset):
+def image_create(request):
     if request.method == 'POST':
         # form is sent
         form = ImageCreateForm(data=request.POST)
         if form.is_valid():
             # form data is valid
-            cd = form.cleaned_data
+            cd = form.cleaned_dat()
             new_item = form.save(commit=False)
             # assign current user to the item
             new_item.user = request.user
