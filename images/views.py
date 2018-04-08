f0b4d82a1700541c469b780917bb9063119d5be5
leonboxer <boxers@126.com>
2018-04-08 18:36:42 +0800

    images

--- images/views.py
+++ images/views.py
@@ -6,24 +6,24 @@ from .forms import ImageCreateForm
 
 @login_required
 def image_create(requset):
-    if requset.method == 'POST':
+    if request.method == 'POST':
         # form is sent
-        form = ImageCreateForm(data=requset.POST)
+        form = ImageCreateForm(data=request.POST)
         if form.is_valid():
             # form data is valid
             cd = form.cleaned_data
             new_item = form.save(commit=False)
             # assign current user to the item
-            new_item.user = requset.user
+            new_item.user = request.user
             new_item.save()
-            messages.success(requset, 'Image added successfully')
+            messages.success(request, 'Image added successfully')
 
             # redirect to new created item detail view
             return redirect(new_item.get_absolute_url())
     else:
         # build form with data provided by the bookmaklet via GET
-        form = ImageCreateForm(data=requset.GET)
-    return render(requset,
+        form = ImageCreateForm(data=request.GET)
+    return render(request,
                   'images/image/create.html',
                   {'sections': 'images',
                    'form': form})
